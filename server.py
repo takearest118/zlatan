d04e1d36c31822d7adf846f5db29de030b0af8dd
EC2 Default User <ec2-user@ip-172-31-18-30.eu-west-3.compute.internal>
2021-10-21 07:09:22 +0000

    update log format

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 import socketserver
+
 
 class MyTCPSocketHandler(socketserver.BaseRequestHandler):
     """
@@ -14,8 +17,7 @@ class MyTCPSocketHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        print("{} {} wrote: {}".format(datetime.utcnow(), self.client_address[0], self.data))
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
 
