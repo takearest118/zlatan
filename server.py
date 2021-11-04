34ab36eaedcf857fc5359472c5889984f60d390d
EC2 Default User <ec2-user@ip-172-31-18-30.eu-west-3.compute.internal>
2021-11-04 10:28:27 +0000

    apply param from client

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ from datetime import datetime
 import random
 
 import socketserver
-
+import requests
 
 
 D_LIST = ('messi', 'ronaldo', 'kangte', 'hwang', 'son')
+
+SLACK_CHANNEL_URL = 'https://hooks.slack.com/services/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'
 
 
 class Singleton(type):
@@ -32,6 +34,23 @@ class MapService(metaclass=Singleton):
         return self.__map
 
 
+class SlackService(metaclass=Singleton):
+
+    def __init__(self, *args, **kwargs):
+        print('init SlackService')
+        self.__url = SLACK_CHANNEL_URL
+
+    def message(self, msg):
+        data = {
+            'channel': '#ringo-log',
+            'username': 'zlatan',
+            'text': msg,
+            'icon_emoji': ':racing_car:'
+        }
+        return requests.post(self.__url, json=data)
+
+
+
 class MyTCPSocketHandler(socketserver.BaseRequestHandler):
     """
     The RequestHandler class for our server.
@@ -45,8 +64,13 @@ class MyTCPSocketHandler(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print("{} {} wrote: {}".format(datetime.utcnow(), self.client_address[0], self.data))
+        k, v = str(self.data, 'utf-8').split(',', 1)
+        MapService().set_device_id(k, v)
+        print(self.client_address)
+        print(dir(self.server))
         MapService().set_device_id(D_LIST[random.randrange(len(D_LIST))], '{},{},{}'.format(random.randint(0, 255), random.randint(0, 255), 0))
         print(MapService().get_map())
+        SlackService().message(str(MapService().get_map()))
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
 
@@ -56,6 +80,7 @@ if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 8888
 
     MapService()
+    SlackService()
 
     # instantiate the server, and bind to localhost on port 8888
     with socketserver.TCPServer((HOST, PORT), MyTCPSocketHandler) as server:
