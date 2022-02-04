29bf411dc965e490509cdef579d0238ecfc6c7c5
Jaehoon Jeong <jaehoon.jeong@monolith.co.kr>
2022-02-04 07:51:47 +0000

    client_input.py: Add shortcut `Ctrl + D`

--- client_input.py
+++ client_input.py
@@ -21,9 +21,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((host, port))
 
     while received != 'disconnected' and received != 'error':
-        data = input('>>> ')
+        try:
+            data = input('>>> ')
+        except EOFError:
+            print("")
+            data = ":/quit"
         sock.sendall(bytes(data + '\n', 'utf-8'))
         received = str(sock.recv(1024), 'utf-8')
         print('Sent:     {}'.format(data))
         print('Received: {}'.format(received))
+            
+            
 
